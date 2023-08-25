0a7023b2f1a03c1060500aa7e202df69d9a0662f
YounesEsbaymi <Headlineryounes2023@gmail.com>
2023-08-25 09:27:37 -0700

    talt

---
+++ _printhex.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int	_printhex(unsigned long int nb)
+{
+	int	len;
+
+	len = 0;
+	if (nb < 16)
+	{
+		if (nb < 10)
+			len += _putchar(nb + 48);
+		else
+			len += _putchar(nb + 87);
+	}
+	if (nb >= 16)
+	{
+		len += _printhex(nb / 16);
+		len += _printhex(nb % 16);
+	}
+	return (len);
+}
